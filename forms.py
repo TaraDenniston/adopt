a4c092f05d6790c8e933e0491517beed68bc0871
Tara Denniston <taradenniston@gmail.com>
2023-02-21 18:16:23 -0500

    add validation to form

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField
+from wtforms import StringField, IntegerField, BooleanField, SelectField
+from wtforms.validators import InputRequired, URL, Optional, NumberRange
 
 
 class AddPetForm(FlaskForm):
-    name = StringField('Pet Name')
-    species = StringField('Species')
-    photo_url = StringField('Photo URL')
-    age = IntegerField('Age in Years')
+    name = StringField('Pet Name', validators=[InputRequired(message="Name is required")])
+    species = SelectField('Species')
+    photo_url = StringField('Photo URL', validators=[URL(), Optional()])
+    age = IntegerField('Age in Years', validators=[NumberRange(min=0, max=30, message='Please enter a number from 0 to 30')])
     notes = StringField('Add details about personality and temperament')
-    available = BooleanField('Available for adoption')
